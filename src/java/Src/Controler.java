53a8275c151fc2b89105c5e8bd28c7a3d939cc9c
snaphoo <gael.renault@epsi.fr>
2017-07-03 10:30:13 +0200

    création methode OpenConnexion et CloseConnexion

--- src/java/Src/Controler.java
+++ src/java/Src/Controler.java
@@ -16,46 +16,49 @@ import java.sql.Statement;
  */
 public class Controler {
     
-    Connection con;
+    private Connection con;
+    private String url = "jdbc:mysql://91.134.132.111:3306/";
+    private String dbName = "JEGYM";
+    private String driver = "com.mysql.jdbc.Driver";
+    private String userName = "JEGYM"; 
+    private String password = "ZCad0wkeps4kauk6";
     
     public Controler(){
-    String url = "jdbc:mysql://91.134.132.111:3306/";
-		  String dbName = "JEGYM";
-		  String driver = "com.mysql.jdbc.Driver";
-		  String userName = "JEGYM"; 
-		  String password = "ZCad0wkeps4kauk6";
-		  try {
-		  Class.forName(driver).newInstance();
-		  this.con = DriverManager.getConnection(url+dbName,userName,password);
-		  
-                  
-                  Statement st = this.con.createStatement();
-		  ResultSet res = st.executeQuery("SELECT * FROM  DT_QUESTION");
-		  while (res.next()) {
-		  int id = res.getInt("ID_QUESTION");
-		  String msg = res.getString("TITRE_QUESTION");
-		  System.out.println(id + "\t" + msg);
-		  }
-                  int val = st.executeUpdate("INSERT into DT_QUESTION(ID_FORMULAIRE, TITRE_QUESTION, DESCRIPTION_QUESTION  ) VALUES(1, 'Un titre', 'Une descr')");
-		  if(val==1)
-			  System.out.print("Successfully inserted value");
-                  
-                  this.con.close();
-		  } catch (Exception e) {
-		  e.printStackTrace();
-		  }           
     }
     public void insertQuestion(int idForm, String titreQuestion, String descQuestion){
-       /* try {                  
-                  Statement st = this.con.createStatement();
-                  int val = st.executeUpdate("INSERT into DT_QUESTION(ID_FORMULAIRE, TITRE_QUESTION, DESCRIPTION_QUESTION  ) VALUES("+idForm+", '"+titreQuestion+"', '"+descQuestion+"')");
-		  if(val==1)System.out.print("Question insérée");
-                  
-                  this.con.close();
-		  } catch (Exception e) {
-		  e.printStackTrace();
-		  }   */
+        try {       
+            OpenConnexion();
+            Statement st = this.con.createStatement();
+            int val = st.executeUpdate("INSERT into DT_QUESTION(ID_FORMULAIRE, TITRE_QUESTION, DESCRIPTION_QUESTION  ) VALUES("+idForm+", '"+titreQuestion+"', '"+descQuestion+"')");
+            if(val==1)System.out.print("Question insérée");
+
+            CloseConnexion();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }   
     }
     
+    public Boolean OpenConnexion(){
+        boolean b = true;
+        try {
+            Class.forName(driver).newInstance();
+            this.con = DriverManager.getConnection(url+dbName,userName,password);
+        } catch (Exception e) {
+            e.printStackTrace();
+            b = false;
+        }  
+        return b;
+    }
+            
+    public Boolean CloseConnexion(){
+        boolean b = true;
+        try {
+            this.con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            b = false;
+        }  
+        return b;
+    }
     
 }
